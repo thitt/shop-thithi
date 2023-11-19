e2b7b82e179a2cf2471bf9353e7a6e9260cca03d
thitt <tranthithi25031996@gmail.com>
2023-11-19 21:18:33 +0700

    feature/thitt_create_migrate

---
+++ database/migrations/2023_11_19_140358_create_shipping_fees_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipping_fees', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('weight_from');
+            $table->decimal('weight_to');
+            $table->decimal('price_per_kg');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipping_fees');
+    }
+};
